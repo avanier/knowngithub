66a5b40b58463964dc11be9ff4f5497d4c6b1506
Alexis Vanier <avanier@users.noreply.github.com>
2017-12-28 12:34:19 -0500

    Towards a first release (#1)
    * relevant commit
    
    * embetter README.md
    
    * better quote :3
    
    * improve README.md
    
    * enhance paranoia
    
    * document all the things! :book:
    
    * properly credit @brentonrails

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Knowngithub
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/knowngithub`. To experiment with that code, run `bin/console` for an interactive prompt.
+> ### _“Some people think this is paranoia, but it isn't. Paranoids only think everyone is out to get them. Wizards know it.”_
+>
+> _&mdash; Terry Pratchett, Sourcery_
 
-TODO: Delete this and the text above, and describe your gem
+Have you hit the problem where you're trying to bootstrap new containers or VMs and `git` will refuse to clone stuff because it doesn't automatically trust GitHub?
+
+This gem answers the scenario where you believe that manually burning the `known_hosts` into whatever you're making doesn't scale, and blindly trusting any host to provide you with legitimiate code is insufficient.
+
+While the solution is not elegant, and the implementation is fragile, I'm hoping it proves the following point : a diagonal chain of trust is better than blind trust.
+
+The idea behind this gem is that by calling GitHub's web pages and API through https, the answers will be certified through it's CA and cannot be tampered with unbeknownst to us. Thus, one can call GitHub's SSH endpoint, and verify its key fingerprint is valid against a dynamic trustable source of truth to protect against man-in-the-middle attacks on `git` operations.
+
+This gem will return one or a list of trusted hosts in a format appropriate for `.ssh/known_hosts` files.
 
 ## Installation
 
@@ -22,7 +32,7 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+The main goal of this gem is for use in automation scripts, specifically [Chef](https://www.chef.io/chef/) cookbooks.
 
 ## Development
 
@@ -30,9 +40,13 @@ After checking out the repo, run `bin/setup` to install dependencies. You can al
 
 To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
 
+## Honorable mentions
+
+Huge thanks to [Brent Smith](https://github.com/BrentOnRails) for the scraping and Nokogiri code.
+
 ## Contributing
 
-Bug reports and pull requests are welcome on GitHub at https://github.com/[USERNAME]/knowngithub.
+Bug reports and pull requests are welcome on GitHub at https://github.com/avanier/knowngithub.
 
 ## License
 
--- lib/knowngithub.rb
+++ lib/knowngithub.rb
@@ -1,5 +1,86 @@
+require 'ipaddr'
+require 'json'
+require 'net/http'
+require 'uri'
+
+require 'net/ssh'
+require 'nokogiri'
 require "knowngithub/version"
 
 module Knowngithub
-  # Your code goes here...
+  # Make a call enforcing the strict use of SSL.
+  # @param [String] url A full url like `https://help.github.com`
+  # @return [Net::HTTP] Return the full Net::HTTP object of the response.
+  # @since 0.1.0
+  def self.safe_call(url)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    http.request(request)
+  end
+
+  # Queries and parses the GitHub help page for the SSH key fingerprints.
+  # @return [Array<String>] SSH key fingerprints as an array of strings.
+  # @since 0.1.0
+  def self.fingerprints
+    pattern = /^(sha256:[a-z0-9\+\/]{43})|([0-9a-f\:]{32,47})$/i
+    res = self.safe_call('https://help.github.com/articles/github-s-ssh-key-fingerprints/')
+    output = Nokogiri::HTML(res.body)
+    fields = output.xpath("//code")
+    return fields.children.map(&:content).select{ |x| pattern =~ x }
+  end
+
+  # Instantiates a Net::SSH session with GitHub to get the host key and closes it.
+  # @return [Net:SSH] Returns a closed Net::SSH session
+  # @since 0.1.0
+  def self.session
+    s = Net::SSH::Transport::Session.new('github.com', { :verify_host_key => true })
+    s.close
+    s
+  end
+
+  # Composes a hash with the properties required for composing a known host entry
+  # @return [Hash] Returns a hash object with all of the needed components to compose a `known_hosts` file
+  # @raise [SecurityError] If the host keys fail validation or if the https call fails, this will be raised.
+  # @since 0.1.0
+  def self.host
+    s = self.session
+    if self.fingerprints.any?{|f| f == s.host_keys.first.fingerprint}
+      base64_key = [Net::SSH::Buffer.from(:key, s.host_keys.first).to_s].pack("m*").gsub(/\s/, "")
+      return {
+        "host_as_string" => s.host_as_string,
+        "ssh_type" => s.host_keys.first.ssh_type,
+        "base64_key" => base64_key
+      }
+    else
+      raise SecurityError # while this is inappropriate, it sounds cool
+    end
+  end
+
+  # Composes a known_hosts entry for the fqdn only
+  # @return [String] Returns a `known_hosts` entry for the fqdn only with no ip address binding as a string.
+  # @since 0.1.0
+  def self.known_host
+    h = self.host
+    return [ h["host_as_string"], h["ssh_type"], h["base64_key"] ].join(' ')
+  end
+
+  # Composes a block of text ready for appending to a known_hosts file
+  # @return [String] Returns a `known_hosts` multiline string enunciating every known IP GitHub may serve SSH on.
+  # @since 0.1.0
+  def self.known_hosts
+    h = self.host
+    cidr_ranges = JSON.parse(self.safe_call('https://api.github.com/meta').body)["git"]
+    known_hosts = []
+    cidr_ranges.each do |range|
+      IPAddr.new(range).to_range.to_a.map { |a| a.to_s }.each do |ip|
+        known_hosts << 'github.com,' + ip + ' ' + h["base64_key"]
+      end
+    end
+    return known_hosts
+  end
 end
