d6672811d4c051fb0696e8c9e9d28d63385dfd61
Alexis Vanier <alexis.vanier@adgear.com>
2018-05-30 10:58:25 -0400

    Provide basic CLI and satisfy the Rubocop

--- knowngithub.gemspec
+++ knowngithub.gemspec
@@ -1,36 +1,37 @@
-lib = File.expand_path("../lib", __FILE__)
+lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "knowngithub/version"
+require 'knowngithub/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "knowngithub"
+  spec.name          = 'knowngithub'
   spec.version       = Knowngithub::VERSION
-  spec.authors       = ["Alexis Vanier"]
-  spec.email         = ["alexis@amonoid.io"]
+  spec.authors       = ['Alexis Vanier']
+  spec.email         = ['alexis@amonoid.io']
 
   spec.summary       = "Don't blindly trust GitHub hosts"
   spec.description   = spec.summary
-  spec.homepage      = "https://github.com/avanier/knowngithub"
-  spec.license       = "MIT"
+  spec.homepage      = 'https://github.com/avanier/knowngithub'
+  spec.license       = 'MIT'
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = 'https://rubygems.org'
+    spec.metadata['allowed_push_host'] = 'https://rubygems.org'
   else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
+    raise 'RubyGems 2.0 or newer is required to protect against ' \
+      'public gem pushes.'
   end
 
-  spec.files         = `git ls-files -z`.split("\x0").reject do |f|
+  spec.files = `git ls-files -z`.split("\x0").reject do |f|
     f.match(%r{^(test|spec|features)/})
   end
-  spec.bindir        = "exe"
+  spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_development_dependency "bundler", "~> 1.16"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_runtime_dependency "net-ssh", "~> 4.2"
-  spec.add_runtime_dependency "nokogiri", "~> 1.8"
+  spec.add_development_dependency 'bundler', '~> 1.16'
+  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_runtime_dependency 'net-ssh', '~> 4.2'
+  spec.add_runtime_dependency 'nokogiri', '~> 1.8'
+  spec.add_runtime_dependency 'thor', '~> 0.20'
 end
--- lib/knowngithub.rb
+++ lib/knowngithub.rb
@@ -5,7 +5,7 @@ require 'uri'
 
 require 'net/ssh'
 require 'nokogiri'
-require "knowngithub/version"
+require 'knowngithub/version'
 
 module Knowngithub
   # Make a call enforcing the strict use of SSL.
@@ -28,17 +28,17 @@ module Knowngithub
   # @since 0.1.0
   def self.fingerprints
     pattern = /^(sha256:[a-z0-9\+\/]{43})|([0-9a-f\:]{32,47})$/i
-    res = self.safe_call('https://help.github.com/articles/github-s-ssh-key-fingerprints/')
+    res = safe_call('https://help.github.com/articles/github-s-ssh-key-fingerprints/')
     output = Nokogiri::HTML(res.body)
-    fields = output.xpath("//code")
-    return fields.children.map(&:content).select{ |x| pattern =~ x }
+    fields = output.xpath('//code')
+    fields.children.map(&:content).select { |x| pattern =~ x }
   end
 
   # Instantiates a Net::SSH session with GitHub to get the host key and closes it.
   # @return [Net:SSH] Returns a closed Net::SSH session
   # @since 0.1.0
   def self.session
-    s = Net::SSH::Transport::Session.new('github.com', { :verify_host_key => true })
+    s = Net::SSH::Transport::Session.new('github.com', verify_host_key: true)
     s.close
     s
   end
@@ -48,13 +48,13 @@ module Knowngithub
   # @raise [SecurityError] If the host keys fail validation or if the https call fails, this will be raised.
   # @since 0.1.0
   def self.host
-    s = self.session
-    if self.fingerprints.any?{|f| f == s.host_keys.first.fingerprint}
-      base64_key = [Net::SSH::Buffer.from(:key, s.host_keys.first).to_s].pack("m*").gsub(/\s/, "")
+    s = session
+    if fingerprints.any? { |f| f == s.host_keys.first.fingerprint }
+      base64_key = [Net::SSH::Buffer.from(:key, s.host_keys.first).to_s].pack('m*').gsub(/\s/, '')
       return {
-        "host_as_string" => s.host_as_string,
-        "ssh_type" => s.host_keys.first.ssh_type,
-        "base64_key" => base64_key
+        'host_as_string' => s.host_as_string,
+        'ssh_type' => s.host_keys.first.ssh_type,
+        'base64_key' => base64_key
       }
     else
       raise SecurityError # while this is inappropriate, it sounds cool
@@ -65,22 +65,22 @@ module Knowngithub
   # @return [String] Returns a `known_hosts` entry for the fqdn only with no ip address binding as a string.
   # @since 0.1.0
   def self.known_host
-    h = self.host
-    return [ h["host_as_string"], h["ssh_type"], h["base64_key"] ].join(' ')
+    h = host
+    [h['host_as_string'], h['ssh_type'], h['base64_key']].join(' ')
   end
 
   # Calls the `/meta` endpoint on GitHub's API via a safe https call and composes an array of entries ready for appending to a known_hosts file.
   # @return [Array<String>] Returns an array of strings enunciating `known_hosts` entries for every known IP GitHub may serve SSH on.
   # @since 0.1.0
   def self.known_hosts
-    h = self.host
-    cidr_ranges = JSON.parse(self.safe_call('https://api.github.com/meta').body)["git"]
+    h = host
+    cidr_ranges = JSON.parse(safe_call('https://api.github.com/meta').body)['git']
     known_hosts = []
     cidr_ranges.each do |range|
-      IPAddr.new(range).to_range.to_a.map { |a| a.to_s }.each do |ip|
-        known_hosts << 'github.com,' + ip + ' ' + h["base64_key"]
+      IPAddr.new(range).to_range.to_a.map(&:to_s).each do |ip|
+        known_hosts << 'github.com,' + ip + ' ' + h['base64_key']
       end
     end
-    return known_hosts
+    known_hosts
   end
 end
